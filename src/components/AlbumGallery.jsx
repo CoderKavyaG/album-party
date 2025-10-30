d0b78ccba3c766f4ad80371f8bb89fd65d135170
CoderKavyaG <codecraftkavya@gmail.com>
2025-10-30 12:42:12 +0530

    fix: add missing useEffect import

--- src/components/AlbumGallery.jsx
+++ src/components/AlbumGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useRef } from 'react'
+import React, { useMemo, useState, useRef, useEffect } from 'react'
 import useSpotifyAlbums from '../hooks/useSpotifyAlbums'
 import Collage from './CollageExport'
 import { logout } from '../utils/spotifyAuth'
